814be03920c81eb67d2ff5c17cf4947360139e7f
yasmin90-web <96856534+yasmin90-web@users.noreply.github.com>
2022-02-07 10:56:13 +0530

    Create hello.js

---
+++ 25 jan/hello.js
@@ -0,0 +1,11 @@
+var a= 8
+if (a==10) {
+    console.log("Given Number is equal to 10");
+}
+else if (a>10){
+      console.log("Given Number is greater than 10");
+}
+else{
+          console.log("Given Number is less than 10");
+}
+© 2022 GitHub, Inc.

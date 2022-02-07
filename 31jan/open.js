c831fd330dcf4ed9a8a4d3c25d338806c49b7ace
yasmin90-web <96856534+yasmin90-web@users.noreply.github.com>
2022-02-07 11:06:28 +0530

    Update open.js

--- 31jan/open.js
+++ 31jan/open.js
@@ -1,51 +1,46 @@
 var obj = {
-    "first_name": "Aayush",
-    "last_name": "Sinha",
-    "email": "aayush.aryan@me.com",
-    "website": "sdfhsdhfjds",
-    "k": "This is Key K's value",
-    "students": [
-	{
-	    "name": "Name 1",
-	    "marks": 1
-	},
-	{
-	    "name": "Name 2",
-	    "marks": 54
-	},
-	{
-	    "name": "Name 3",
-	    "marks": 44
-	},
-	{
-	    "name": "Name 4",
-	    "marks": 3
-	},
-	{
-	    "name": "Name 5",
-	    "marks": 5
-	},
-        {
-	    "name": "Name 6",
-	    "marks": 23
-	},
-    ],
+   "students" : [
+     {
+       "name":"virat",
+       "marks": 15
+     },
+     {
+       "name":"rohit",
+       "marks":17
+     },
+     {
+     "name":"shikhar",
+     "marks":19
+     },
+     {
+    "name":"k1 rahul",
+      "marks":21
+},
+  {
+ "name":"Ms dhoni",
+  "marks": 0
+},
+  {
+  "name":"jasprit",
+    "marks":27
+  },
+     {
+       "name":"hardik pandya",
+       "marks":24
+     },
+     {
+      "name":"aheteshwar pujara",
+       "marks":19
+     },
+     {
+       "name":"ajinkya",
+       "marks":23
+     },
+     ],
 };
-
-pass = prompt("Enter marks");
-pass = parseInt(pass)
-
-len = obj.students.length
-
-var passed_stu = [];
-
-for (i=0; i<len; i++){
-    new1 = obj.students[i];
-    
-    if(new1["marks"] >= pass){
-        passed_stu.push(new1["name"]);
-    }
-
-}
-
-console.log(passed_stu);
+var yourNumber = prompt("enter the score")
+var studentsLength = obj.students.length
+for(var i=0;i<studentsLength;i=i+1){
+  if (yourNumber < obj.students[i].marks ){
+    console.log(obj.students[i].name)
+  }
